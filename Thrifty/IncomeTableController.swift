83e91a7e1af2827e473149b94a3254c1284451bb
boristeo <boristeodorovich@gmail.com>
2017-06-15 05:15:41 -0700

    additional files I may have changed

--- Thrifty/IncomeTableController.swift
+++ Thrifty/IncomeTableController.swift
@@ -1,29 +1,31 @@
 //
-//  TVController.swift
-//  CIS55Lab2_Boris
+//  IncomeTableController.swift
+//  Thrifty
 //
-//  Created by Boris Teodorovich on 5/15/17.
-//  Copyright © 2017 Boris Teodorovich. All rights reserved.
+//  Created by Boris Teodorovich on 6/15/17.
+//  Copyright © 2017 DeAnza. All rights reserved.
 //
 
 import UIKit
 import CoreData
 
 
-class ExpenseTableController: UITableViewController, NSFetchedResultsControllerDelegate {
+class IncomeTableController: UITableViewController, NSFetchedResultsControllerDelegate {
     
     
     
     
     // Stuff for the section labels
-    var expenseDict = [String: [ExpenseMO]]()
+    var incomeDict = [String: [IncomeMO]]()
     var sectionTitles = [String]()
-
+    
     
     
     // Auto Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+
+
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -63,9 +65,9 @@ class ExpenseTableController: UITableViewController, NSFetchedResultsControllerD
         default:
             tableView.reloadData()
         }
-//        
+        
 //        if let fetchedObjects = controller.fetchedObjects {
-//            expenses = fetchedObjects as! [ExpenseMO]
+//            expenses = fetchedObjects as! [IncomeMO]
 //        }
     }
     
@@ -78,24 +80,24 @@ class ExpenseTableController: UITableViewController, NSFetchedResultsControllerD
     
     
     // Sections
-
-//    // On the side
-//    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-////        if searchController.isActive {
-////            return [""]
-////        }
-////        else {
-//            return sectionTitles
-////        }
-//    }
+    
+    //    // On the side
+    //    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+    ////        if searchController.isActive {
+    ////            return [""]
+    ////        }
+    ////        else {
+    //            return sectionTitles
+    ////        }
+    //    }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-            return sectionTitles.count
+        return sectionTitles.count
     }
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let values = expenseDict[sectionTitles[section]] {
+        if let values = incomeDict[sectionTitles[section]] {
             return values.count
         }
         else {
@@ -104,7 +106,7 @@ class ExpenseTableController: UITableViewController, NSFetchedResultsControllerD
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-            return sectionTitles[section]
+        return sectionTitles[section]
     }
     
     override func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
@@ -118,10 +120,10 @@ class ExpenseTableController: UITableViewController, NSFetchedResultsControllerD
     
     // Cells
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellID = "ExpenseCell"
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! ExpenseCell
+        let cellID = "IncomeCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! IncomeCell
         
-        if let values = expenseDict[sectionTitles[indexPath.section]] {
+        if let values = incomeDict[sectionTitles[indexPath.section]] {
             let cellItem = values[indexPath.row]
             updateCellContents(in: cell, with: cellItem)
         }
@@ -136,20 +138,21 @@ class ExpenseTableController: UITableViewController, NSFetchedResultsControllerD
         
         switch editingStyle {
         case .delete:
-            let context = getContext()
-            if let itemToDelete = expenseDict[sectionTitles[indexPath.section]]?[indexPath.row] {
-                context.delete(itemToDelete)
-            }
-            try! context.save()
-            fetchFromCD()
-            tableView.reloadData()
+            
+                let context = getContext()
+                if let itemToDelete = incomeDict[sectionTitles[indexPath.section]]?[indexPath.row] {
+                    context.delete(itemToDelete)
+                }
+                try! context.save()
+                fetchFromCD()
+                tableView.reloadData()
             
         default: break
         }
     }
     
     
-
+    
     
     
     
@@ -162,41 +165,40 @@ class ExpenseTableController: UITableViewController, NSFetchedResultsControllerD
     
     
     func fetchFromCD() {
-        expenseDict = [:]
+        incomeDict = [:]
         sectionTitles = []
-        
+
         if let user = UserMO.userWithName("default", inMOContext: getContext()) {
-            for expense in user.spends! {
-                addToDict(expense as! ExpenseMO)
+            for income in user.receives! {
+                addToDict(income as! IncomeMO)
             }
         }
         
-
+        
     }
     
-    func addToDict(_ expense: ExpenseMO) {
+    func addToDict(_ expense: IncomeMO) {
         if let key = expense.type {
             
-            if var valsWithSameFirstLetter = expenseDict[key] {
+            if var valsWithSameFirstLetter = incomeDict[key] {
                 valsWithSameFirstLetter.append(expense)
-                expenseDict[key] = valsWithSameFirstLetter
+                incomeDict[key] = valsWithSameFirstLetter
             }
             else {
-                expenseDict[key] = [expense]
+                incomeDict[key] = [expense]
             }
         }
         
-        sectionTitles = [String](expenseDict.keys)
+        sectionTitles = [String](incomeDict.keys)
         sectionTitles.sort()
     }
-
     
-    func updateCellContents(in cell: ExpenseCell, with cellItem: ExpenseMO) {
+    
+    func updateCellContents(in cell: IncomeCell, with cellItem: IncomeMO) {
         cell.nameField.text = cellItem.descr
         cell.daysPeriodField.text = String(cellItem.daysCycle)
         cell.amountField.text = String(cellItem.amount)
-
+        
     }
     
 }
-
